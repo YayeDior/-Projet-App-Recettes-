d89a91718b63a9f00cb48bf52743551bf0c36bca
YayeDior <diordogo97@gmail.com>
2021-04-27 11:47:18 +0200

    force push

---
+++ src/navigations/Navigator.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import {createStackNavigator} from '@react-navigation/stack'
+import List from '../screens/List'
+import Detail from '../screens/Detail'
+
+const Stack = createStackNavigator();
+const screenOptionStyle = {
+    headerShown:false
+}
+
+const HomeStackNavigator = () => {
+    return(
+        <Stack.Navigator screenOptions={screenOptionStyle}>
+            <Stack.Screen name="List" component={List}/>
+            <Stack.Screen name="Detail" component={Detail}/>
+        </Stack.Navigator>
+    )
+}
+export default HomeStackNavigator;
---
+++ src/redux/reducers/recipesList.js
@@ -0,0 +1,8 @@
+const defaultState = [];
+
+export const recipesList = (state = defaultState,actions) => {switch (actions.type)
+{
+    default:
+        return state;
+}
+};
